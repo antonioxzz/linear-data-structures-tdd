24037e076d71ccf8a05e8ec87f9301bac41bdda1
antonioxzz <antonioitalo.xzz@gmail.com>
2023-05-27 17:49:38 -0500

    testing stack1

---
+++ StackExercises/Excercise1/test/test_stack1.py
@@ -0,0 +1,18 @@
+import pytest
+from stack1.stack1 import are_parentheses_balanced
+
+
+@pytest.mark.parametrize("expression, expected", [
+    ("()", True),
+    ("()[]{}", True),
+    ("({[]})", True),
+    ("(", False),
+    ("[)", False),
+    ("(]", False),
+    ("([)]", False),
+    ("{[}]", False),
+    ("[{)]}", False),
+])
+def test_are_parentheses_balanced(expression, expected):
+    result = are_parentheses_balanced(expression)
+    assert result == expected
